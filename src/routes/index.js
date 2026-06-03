5fefc888ac80fabab902b980d1f3cb8bcd79dad8
JulioRiveraAguilar <161143868+JulioRiveraAguilar@users.noreply.github.com>
2026-06-03 13:44:02 -0600

    feat: agregado de rutas de clientes al index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const authRoutes = require('./authRoutes');
 const usuariosRoutes = require('./usuariosRoutes');
+const clientesRoutes = require('./clientesRoutes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/usuarios', usuariosRoutes);
+router.use('/clientes', clientesRoutes);
 
 module.exports = router;
