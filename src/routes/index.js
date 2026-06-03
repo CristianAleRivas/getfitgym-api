283237bb833e78fdc89e91990cd2bf3243303fcb
JulioRiveraAguilar <161143868+JulioRiveraAguilar@users.noreply.github.com>
2026-06-03 13:50:16 -0600

    feat: agregado de rutas de membresias al index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const authRoutes = require('./authRoutes');
 const usuariosRoutes = require('./usuariosRoutes');
 const clientesRoutes = require('./clientesRoutes');
+const membresiasRoutes = require('./membresiasRoutes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/usuarios', usuariosRoutes);
 router.use('/clientes', clientesRoutes);
+router.use('/membresias', membresiasRoutes);
 
 module.exports = router;
